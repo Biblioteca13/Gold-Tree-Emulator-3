110e277e34f3e8ed504cb27b16c326e6fa2f0d19
JunioriRetro <juniori.retro@hotmail.com>
2014-02-06 00:29:15 +0200

    Fixed friend_stream sql no default value

--- SQL FILES/ImagicianPHXEDITtoGTE.sql
+++ SQL FILES/ImagicianPHXEDITtoGTE.sql
@@ -190,8 +190,8 @@ CREATE TABLE IF NOT EXISTS `friend_stream` (
   `gender` enum('M','F') NOT NULL DEFAULT 'M',
   `look` varchar(255) NOT NULL DEFAULT 'hr-115-42.hd-190-1.ch-215-62.lg-285-91.sh-290-62	',
   `time` double NOT NULL DEFAULT '0',
-  `data` varchar(255) NOT NULL,
-  `data_extra` varchar(255) NOT NULL,
+  `data` varchar(255) NOT NULL NULL DEFAULT '',
+  `data_extra` varchar(255) NOT NULL NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
